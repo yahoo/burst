732f624a7df31b86d7a6d3b85e7b5bc0350b3793
Clay Jensen-Reimann <clayreimann@yahooinc.com>
2023-08-21 11:23:19 -0500

    Check otel config for trex exporter

---
+++ burst-vitals/src/main/java/org/burstsys/vitals/reporter/BurstAutoConfigurationCustomizerProvider.java
@@ -0,0 +1,30 @@
+package org.burstsys.vitals.reporter;
+
+import com.google.auto.service.AutoService;
+import io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizer;
+import io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizerProvider;
+import io.opentelemetry.sdk.autoconfigure.spi.ConfigProperties;
+import io.opentelemetry.sdk.trace.SdkTracerProviderBuilder;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.Collections;
+
+
+@SuppressWarnings("unused")
+@AutoService(AutoConfigurationCustomizerProvider.class)
+public class BurstAutoConfigurationCustomizerProvider implements AutoConfigurationCustomizerProvider {
+
+    @Override
+    @ParametersAreNonnullByDefault
+    public void customize(AutoConfigurationCustomizer autoConfigurationCustomizer) {
+        autoConfigurationCustomizer.addTracerProviderCustomizer(this::addBurstTracers);
+        autoConfigurationCustomizer.addPropagatorCustomizer();
+    }
+
+    private SdkTracerProviderBuilder addBurstTracers(SdkTracerProviderBuilder tracerProvider, ConfigProperties config) {
+        if (!config.getList("otel.traces.exporter", Collections.emptyList()).contains("trek"))
+            return tracerProvider;
+        return tracerProvider.addSpanProcessor(TrekSpanProcessor.create(TrekLoggingSpanExporter.create()));
+    }
+
+}
