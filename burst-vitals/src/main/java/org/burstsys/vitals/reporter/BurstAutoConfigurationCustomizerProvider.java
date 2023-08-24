b9e73cedff6051ae51191b220d3164258e8e0303
Clay Jensen-Reimann <clayreimann@yahooinc.com>
2023-08-24 11:11:21 -0500

    Update BurstAutoConfigurationCustomizerProvider.java

--- burst-vitals/src/main/java/org/burstsys/vitals/reporter/BurstAutoConfigurationCustomizerProvider.java
+++ burst-vitals/src/main/java/org/burstsys/vitals/reporter/BurstAutoConfigurationCustomizerProvider.java
@@ -8,21 +8,26 @@ import io.opentelemetry.sdk.trace.SdkTracerProviderBuilder;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.Collections;
+import java.util.logging.Logger;
 
 
 @SuppressWarnings("unused")
 @AutoService(AutoConfigurationCustomizerProvider.class)
 public class BurstAutoConfigurationCustomizerProvider implements AutoConfigurationCustomizerProvider {
 
+    private static final Logger logger = Logger.getLogger(BurstAutoConfigurationCustomizerProvider.class.getName());
+
     @Override
     @ParametersAreNonnullByDefault
     public void customize(AutoConfigurationCustomizer autoConfigurationCustomizer) {
+        logger.info("BurstAutoConfigurationCustomizerProvider will customize otel");
         autoConfigurationCustomizer.addTracerProviderCustomizer(this::addBurstTracers);
     }
 
     private SdkTracerProviderBuilder addBurstTracers(SdkTracerProviderBuilder tracerProvider, ConfigProperties config) {
-        if (!config.getList("otel.traces.exporter", Collections.emptyList()).contains("trek"))
-            return tracerProvider;
+//        if (!config.getList("otel.traces.exporter", Collections.emptyList()).contains("trek"))
+//            return tracerProvider;
+        logger.info("TrekLoggingSpanExporter is enabled");
         return tracerProvider.addSpanProcessor(TrekSpanProcessor.create(TrekLoggingSpanExporter.create()));
     }
 
